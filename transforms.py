a7dacf9d6abe35d00f0e423510ab185ade09deed
superporchetta <35531309+superporchetta@users.noreply.github.com>
2022-03-11 12:01:42 +0100

    Create transforms.py

---
+++ transforms.py
@@ -0,0 +1,25 @@
+from sklearn.preprocessing import StandardScaler, MaxAbsScaler
+
+# https://pytorch.org/tutorials/beginner/data_loading_tutorial.html
+
+
+class StandardScaler(object):
+    """Applies the standard scaler form sklearn to the dataset
+    """
+
+    def __init__(self, output_size):
+        return 0
+
+    def __call__(self, sample):
+        return 0
+
+
+class MaxAbsScaler(object):
+    """Applies the MaxAbs Scaler from Sklearn
+    """
+
+    def __init__(self, output_size):
+        return 0
+
+    def __call__(self, sample):
+        return 0
